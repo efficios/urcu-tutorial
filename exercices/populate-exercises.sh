d706aba202334f7f5d52fa162a98e3c7038f0db1
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2013-10-17 18:35:21 -0400

    update exercises
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- exercices/populate-exercises.sh
+++ exercices/populate-exercises.sh
@@ -3,10 +3,15 @@
 TOP_DIR=$(pwd)
 GAME_DIR=${TOP_DIR}/../urcu-game
 
+mkdir ${TOP_DIR}/questions/
+mkdir ${TOP_DIR}/answers/
+
 for a in $(cat exercises.list); do
-	mkdir ${TOP_DIR}/questions/${a}
-	cp ${GAME_DIR}/*.[ch] ${TOP_DIR}/questions/${a}
-	cp ${GAME_DIR}/Makefile ${TOP_DIR}/questions/${a}
-	cd ${TOP_DIR}/questions/${a}
-	patch -p2 < ${TOP_DIR}/patches/${a}.patch
+	mkdir ${TOP_DIR}/${a}
+	cp ${GAME_DIR}/*.[ch] ${TOP_DIR}/${a}
+	cp ${GAME_DIR}/Makefile ${TOP_DIR}/${a}
+	cd ${TOP_DIR}/${a}
+	if [[ -f ${TOP_DIR}/patches/${a}.patch ]] ; then
+		patch -p2 < ${TOP_DIR}/patches/${a}.patch
+	fi
 done
